20f51fb46d38cd5d5ee199d9509b6f5e29ab5705
k-l-lambda <k.l.lambda@gmail.com>
2017-08-07 11:35:37 +0800

    BoneMarker.cs added.

---
+++ Pinist Mobile/Assets/Scripts/BoneMarker.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[ExecuteInEditMode]
+public class BoneMarker : MonoBehaviour
+{
+
+	public Transform Tip;
+	public float Width = 0.6f;
+	public bool AutoUpdateMesh = false;
+
+	// Use this for initialization
+	void Start () {
+		if (AutoUpdateMesh)
+			updateMesh();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void updateMesh() {
+		const float sqrt3_3 = 0.57735f;
+		float half_wid = Width / 2f;
+		Vector3 tip_pos = Tip.localPosition;
+
+		Vector3[] vertices = new Vector3[] {
+			new Vector3(-half_wid, -half_wid, 0), new Vector3(-half_wid, +half_wid, 0), new Vector3(+half_wid, +half_wid, 0), new Vector3(+half_wid, -half_wid, 0),
+			tip_pos + new Vector3(-half_wid, -half_wid, 0), tip_pos + new Vector3(-half_wid, +half_wid, 0), tip_pos + new Vector3(+half_wid, +half_wid, 0), tip_pos + new Vector3(+half_wid, -half_wid, 0),
+		};
+		Vector3[] normals = new Vector3[] {
+			new Vector3(-sqrt3_3, -sqrt3_3, -sqrt3_3), new Vector3(-sqrt3_3, +sqrt3_3, -sqrt3_3), new Vector3(+sqrt3_3, +sqrt3_3, -sqrt3_3), new Vector3(+sqrt3_3, -sqrt3_3, -sqrt3_3),
+			new Vector3(-sqrt3_3, -sqrt3_3, +sqrt3_3), new Vector3(-sqrt3_3, +sqrt3_3, +sqrt3_3), new Vector3(+sqrt3_3, +sqrt3_3, +sqrt3_3), new Vector3(+sqrt3_3, -sqrt3_3, +sqrt3_3),
+		};
+
+		Mesh box = new Mesh();
+		box.vertices = vertices;
+		box.normals = normals;
+
+		box.subMeshCount = 3;
+		box.SetIndices(new int[] { 0, 1, 2, 3, 0 }, MeshTopology.LineStrip, 0);
+		box.SetIndices(new int[] { 4, 5, 6, 7, 4 }, MeshTopology.LineStrip, 1);
+		box.SetIndices(new int[] { 0, 4, 1, 5, 2, 6, 3, 7 }, MeshTopology.Lines, 2);
+
+		MeshFilter mf = GetComponent<MeshFilter>();
+		if (mf)
+			mf.mesh = box;
+	}
+}
