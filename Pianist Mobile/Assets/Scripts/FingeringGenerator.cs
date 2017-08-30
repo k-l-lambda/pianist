b5c52f82e3911b2bb074a53552009be1eef88513
k-l-lambda <k.l.lambda@gmail.com>
2017-08-30 22:15:49 +0800

    FingeringGenerator: added method of clear. FingeringGenerator: added note field in marker.

--- Pianist Mobile/Assets/Scripts/FingeringGenerator.cs
+++ Pianist Mobile/Assets/Scripts/FingeringGenerator.cs
@@ -46,6 +46,40 @@ public class FingeringGenerator : MonoBehaviour
 		Debug.Log("MIDI file saved: " + fileName);
 	}
 
+	public static void clear(Midi.Sequence seq)
+	{
+		foreach (Midi.Track track in seq)
+		{
+			System.Collections.Generic.List<int> toRemove = new System.Collections.Generic.List<int>();
+
+			for (int i = track.Count - 1; i >= 0; --i)
+			{
+				Midi.MidiEvent e = track.GetMidiEvent(i);
+
+				if (e.MidiMessage.MessageType == Midi.MessageType.Meta)
+				{
+					Midi.MetaMessage msg = e.MidiMessage as Midi.MetaMessage;
+					switch (msg.MetaType)
+					{
+						case Midi.MetaType.Text:
+							if (new System.Text.RegularExpressions.Regex("^Fingering").Match(System.Text.Encoding.Default.GetString(msg.GetBytes())).Length > 0)
+								toRemove.Add(i);
+
+							break;
+						case Midi.MetaType.Marker:
+							if (new System.Text.RegularExpressions.Regex("^finger:").Match(System.Text.Encoding.Default.GetString(msg.GetBytes())).Length > 0)
+								toRemove.Add(i);
+
+							break;
+					}
+				}
+			}
+
+			foreach (int i in toRemove)
+				track.RemoveAt(i);
+		}
+	}
+
 	public static void run(Midi.Sequence seq)
 	{
 		if (seq.Count == 0)
@@ -54,7 +88,9 @@ public class FingeringGenerator : MonoBehaviour
 			return;
 		}
 
-		seq[0].Insert(0, new Midi.MetaMessage(Midi.MetaType.Text, System.Text.Encoding.Default.GetBytes("Fingering by K.L.Pianist, fingering-marker-pattern:finger:([\\d,]+)")));
+		clear(seq);
+
+		seq[0].Insert(0, new Midi.MetaMessage(Midi.MetaType.Text, System.Text.Encoding.Default.GetBytes("Fingering by K.L.Pianist, fingering-marker-pattern:finger:(\\d+)\\|([\\d,]+)")));
 
 		foreach (Midi.Track track in seq)
 		{
@@ -69,7 +105,7 @@ public class FingeringGenerator : MonoBehaviour
 						if (f >= 0)
 							f += 1;
 
-						string marker = string.Format("finger:{0}", f);
+						string marker = string.Format("finger:{0}|{1}", cm.Data1, f);
 
 						track.Insert(e.AbsoluteTicks, new Midi.MetaMessage(Midi.MetaType.Marker, System.Text.Encoding.Default.GetBytes(marker)));
 					}
