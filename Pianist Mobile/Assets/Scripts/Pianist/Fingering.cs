013e074b10f347bd819a0d798dc456a7ea34da13
k-l-lambda <k.l.lambda@gmail.com>
2017-09-06 18:18:45 +0800

    Pianist/Fingering: FingerConstants added.

--- Pianist Mobile/Assets/Scripts/Pianist/Fingering.cs
+++ Pianist Mobile/Assets/Scripts/Pianist/Fingering.cs
@@ -94,4 +94,79 @@ namespace Pianist
 		LEFT,
 		RIGHT,
 	}
+
+
+	public class FingerConstants
+	{
+		public struct Bound
+		{
+			public int left;
+			public int right;
+
+			public Bound(Finger finger)
+			{
+				int value = (int)finger;
+				if (System.Math.Abs(value) < 10)
+				{
+					left = value;
+					right = value;
+				}
+				else
+				{
+					bool minus = value < 0;
+					value = System.Math.Abs(value);
+
+					left = (int)System.Math.Floor(value / 10f);
+					right = value % 10;
+				}
+			}
+		};
+
+		public static readonly Dictionary<Finger, Bound> Bounds = new Dictionary<Finger, Bound>()
+		{
+			{Finger.EMPTY,					new Bound(Finger.EMPTY)},
+
+			{Finger.LEFT_THUMB			,	new Bound(Finger.LEFT_THUMB)},
+			{Finger.LEFT_INDEX			,	new Bound(Finger.LEFT_INDEX)},
+			{Finger.LEFT_MIDDLE			,	new Bound(Finger.LEFT_MIDDLE)},
+			{Finger.LEFT_RING			,	new Bound(Finger.LEFT_RING)},
+			{Finger.LEFT_PINKY			,	new Bound(Finger.LEFT_PINKY)},
+
+			{Finger.RIGHT_THUMB			,	new Bound(Finger.RIGHT_THUMB)},
+			{Finger.RIGHT_INDEX			,	new Bound(Finger.RIGHT_INDEX)},
+			{Finger.RIGHT_MIDDLE		,	new Bound(Finger.RIGHT_MIDDLE)},
+			{Finger.RIGHT_RING			,	new Bound(Finger.RIGHT_RING)},
+			{Finger.RIGHT_PINKY			,	new Bound(Finger.RIGHT_PINKY)},
+
+			{Finger.LEFT_THUMB_INDEX	,	new Bound(Finger.LEFT_THUMB_INDEX)},
+			{Finger.LEFT_THUMB_MIDDLE	,	new Bound(Finger.LEFT_THUMB_MIDDLE)},
+			{Finger.LEFT_THUMB_RING		,	new Bound(Finger.LEFT_THUMB_RING)},
+			{Finger.LEFT_THUMB_PINKY	,	new Bound(Finger.LEFT_THUMB_PINKY)},
+			{Finger.LEFT_INDEX_MIDDLE	,	new Bound(Finger.LEFT_INDEX_MIDDLE)},
+			{Finger.LEFT_MIDDLE_RING	,	new Bound(Finger.LEFT_MIDDLE_RING)},
+			{Finger.LEFT_RING_PINKY		,	new Bound(Finger.LEFT_RING_PINKY)},
+			{Finger.LEFT_INDEX_THUMB	,	new Bound(Finger.LEFT_INDEX_THUMB)},
+			{Finger.LEFT_MIDDLE_THUMB	,	new Bound(Finger.LEFT_MIDDLE_THUMB)},
+			{Finger.LEFT_RING_THUMB		,	new Bound(Finger.LEFT_RING_THUMB)},
+			{Finger.LEFT_PINKY_THUMB	,	new Bound(Finger.LEFT_PINKY_THUMB)},
+			{Finger.LEFT_MIDDLE_INDEX	,	new Bound(Finger.LEFT_MIDDLE_INDEX)},
+			{Finger.LEFT_RING_MIDDLE	,	new Bound(Finger.LEFT_RING_MIDDLE)},
+			{Finger.LEFT_PINKY_RING		,	new Bound(Finger.LEFT_PINKY_RING)},
+
+			{Finger.RIGHT_THUMB_INDEX	,	new Bound(Finger.RIGHT_THUMB_INDEX)},
+			{Finger.RIGHT_THUMB_MIDDLE	,	new Bound(Finger.RIGHT_THUMB_MIDDLE)},
+			{Finger.RIGHT_THUMB_RING	,	new Bound(Finger.RIGHT_THUMB_RING)},
+			{Finger.RIGHT_THUMB_PINKY	,	new Bound(Finger.RIGHT_THUMB_PINKY)},
+			{Finger.RIGHT_INDEX_MIDDLE	,	new Bound(Finger.RIGHT_INDEX_MIDDLE)},
+			{Finger.RIGHT_MIDDLE_RING	,	new Bound(Finger.RIGHT_MIDDLE_RING)},
+			{Finger.RIGHT_RING_PINKY	,	new Bound(Finger.RIGHT_RING_PINKY)},
+			{Finger.RIGHT_INDEX_THUMB	,	new Bound(Finger.RIGHT_INDEX_THUMB)},
+			{Finger.RIGHT_MIDDLE_THUMB	,	new Bound(Finger.RIGHT_MIDDLE_THUMB)},
+			{Finger.RIGHT_RING_THUMB	,	new Bound(Finger.RIGHT_RING_THUMB)},
+			{Finger.RIGHT_PINKY_THUMB	,	new Bound(Finger.RIGHT_PINKY_THUMB)},
+			{Finger.RIGHT_MIDDLE_INDEX	,	new Bound(Finger.RIGHT_MIDDLE_INDEX)},
+			{Finger.RIGHT_RING_MIDDLE	,	new Bound(Finger.RIGHT_RING_MIDDLE)},
+			{Finger.RIGHT_PINKY_RING	,	new Bound(Finger.RIGHT_PINKY_RING)},
+		};
+	};
 }
