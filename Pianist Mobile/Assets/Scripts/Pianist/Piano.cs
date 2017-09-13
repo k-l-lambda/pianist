9f90d3ecf18299804493f015fcb06b093f9d1184
k-l-lambda <k.l.lambda@gmail.com>
2017-09-13 10:19:32 +0800

    Pianist/Piano: KeyPositions added.

--- Pianist Mobile/Assets/Scripts/Pianist/Piano.cs
+++ Pianist Mobile/Assets/Scripts/Pianist/Piano.cs
@@ -1,12 +1,39 @@
 
+using Math = System.Math;
+using System.Linq;
+
 
 namespace Pianist
 {
 	public class Piano
 	{
-		public static readonly float[] KeyPositions =
+		public static readonly int GroupLength = 12;
+		public static readonly int GroupSize = 7;
+
+
+		public static readonly float[] GroupKeyPositions =
 		{
-			0, 0.38f, 1, 1.62f, 2, 3, 3.37f, 4, 4.50f, 5, 5.63f, 6,
+			0, 0.38f, 1, 1.62f, 2, 3, 3.37f, 4, 4.50f, 5, 5.63f, 6, 7,
 		};
+
+		public static int pitchStep(int pitch)
+		{
+			return pitch % GroupLength;
+		}
+
+		public static int pitchGroup(int pitch)
+		{
+			return pitch / GroupLength;
+		}
+
+		public static float pitchPosition(int pitch)
+		{
+			int step = pitchStep(pitch);
+			int group = pitchGroup(pitch);
+
+			return group * GroupSize + GroupKeyPositions[step];
+		}
+
+		public static readonly float[] KeyPositions = Enumerable.Range(0, 120).Select(x => pitchPosition(x)).ToArray();
 	};
 }
