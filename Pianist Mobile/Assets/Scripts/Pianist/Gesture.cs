92124db8edcd6ceeaba7b4ca6f6b82cb367bfcdd
K.L <k.l.lambda@gmail.com>
2017-10-13 10:37:16 +0800

    Gesture.cs added.

---
+++ Pianist Mobile/Assets/Scripts/Pianist/Gesture.cs
@@ -0,0 +1,24 @@
+
+
+namespace Pianist
+{
+	public class FingerGesture
+	{
+		public float[] angles;
+
+		public double Cost
+		{
+			get;
+		}
+	};
+
+	public class HandGesture
+	{
+		public float[] angles;
+
+		public double Cost
+		{
+			get;
+		}
+	};
+}
